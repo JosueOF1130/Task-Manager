d3a601140faf5b0eae6f839db62c0a958a9a957d
Josue <josue.orofre@gmail.com>
2025-04-21 17:34:53 -0700

    added styling for tasks, icons and edit ability

--- root/js/tasks.js
+++ root/js/tasks.js
@@ -1,28 +1,42 @@
 // 1. IMPORTS
-import { db, auth, onAuthStateChanged, getDoc, doc, addDoc, collection} from "./db.js";
-import { Logout } from "./auth.js";
+import { db, auth, onAuthStateChanged, getDoc, doc, addDoc, collection, getDocs, updateDoc, deleteDoc } from "./db.js";
+// import { Logout } from "./auth.js";
+import Icons from "./icons.js";
+import { CreateTask, GetTasks, DeleteTask } from "./crud.js";
 // 2. GLOBAL VARIABLES
 
 const greetViewBox = document.getElementById("greeting");
-const logoutBtn = document.getElementById("logout-btn");   
+const logoutBtn = document.getElementById("logout-btn");
+const addTaskBtn = document.getElementById("add-task-btn");
+
+const modalContainer = document.getElementById("edit-task-modal");
 
 // 3. FUNCTIONS
+
+//check if user is logged in
+
+function CheckUser() {
+    const user = auth.currentUser; 
+    if(!user) {
+        //TODO show error toast
+        // alert("User not signed in");
+        return null;
+    }
+    // console.log("check user: ", user);
+    return user;
+}
+
 
 //CRUD operations for tasks
 
 //create task
 async function AddTask() {
+    
+    const taskField = document.getElementById("input-task");
+    const priorityButton = document.querySelector('input[name="priority"]:checked');
 
-    const user = auth.currentUser;
-    if(!user) {
-        //TODO show error toast
-        alert("User not signed in");
-        return;
-    }
-
-
-    const task = document.getElementById("input-task").value;
-    const priority = document.querySelector('input[name="priority"]:checked').value;
+    let task = taskField.value;
+    let priority = priorityButton.value;
 
     if (!task || !priority) {
         // TODO show error toast
@@ -46,14 +60,25 @@ async function AddTask() {
             priorityNumber = 2;
     }
     try {
-        const tasksCollectionRef = collection(db, "users", user.uid, "tasks");
-        await addDoc(tasksCollectionRef, {
-            task: task,
-            priority: priorityNumber,
-            completed: false
-        });
+        const newTask = await CreateTask(task, priorityNumber);
         //TODO show success toast
         alert("Task added successfully");
+
+        //manually add task to the list
+        const taskList = document.getElementById("task-list");
+        const markup = `<li class="task-item p-2 d-flex align-items-center justify-content-between " data-id="${newTask.id}">
+        <div class="task-check">
+            <input type="checkbox" class="form-check-input me-2" id="task-${newTask.id}">
+            <label for="task-${newTask.id}">${task}</label>
+        </div>
+        <button class="trash-btn btn p-2" id="delete-task-${newTask.id}">${Icons.trashIcon}</button>
+    </li>`;
+    taskList.innerHTML += markup;
+
+    //clear input field
+    taskField.value = "";
+    document.getElementById("medium-priority").checked = true;
+
     } catch (error) { 
         //TODO show error toast
         console.log(error.message);
@@ -63,16 +88,64 @@ async function AddTask() {
 
 //read tasks
 async function ReadTasks() {
-    const user = auth.currentUser; 
-    if(!user) {
+    try {
+        const tasks = await GetTasks();
+        let markup = "";
+        tasks.forEach((task) => {
+            const data = task.data();
+            console.log(data);
+            console.log(data.completed)
+            markup += `<li class="task-item p-2 d-flex align-items-center justify-content-between " data-id="${task.id}">
+            <div class="task-check d-flex align-items-center">
+                <input type="checkbox" class="form-check-input me-2 mt-0" id="task-${task.id}" ${data.completed ? "checked" : ""}>
+                <label for="task-${task.id}">${data.task}</label>
+                ${ data.priority == 1 ? Icons.lowPriorityIcon : data.priority == 2 ? Icons.midPriorityIcon : Icons.highPriorityIcon}
+            </div>
+
+            <div class="btn-group" role="group" aria-label="edit and delete buttons">
+                <button type="button" class="edit-btn btn w-50 p-2" id="edit-task-${task.id}" data-bs-toggle="modal" data-bs-target="#edit-task-modal">${Icons.editIcon}</button>
+                <button type="button" class="trash-btn btn p-2" id="delete-task-${task.id}">${Icons.trashIcon}</button>
+            </div>
+
+            
+            </li>`;
+        });
+
+        const taskList = document.getElementById("task-list");
+        taskList.innerHTML = markup;
+        //checkbox event listener
+        tasks.forEach((task) => {
+            const checkbox = document.getElementById(`task-${task.id}`);
+            checkbox.addEventListener("change", () => ToggleTask(task.id));
+            const deleteBtn = document.getElementById(`delete-task-${task.id}`);
+            deleteBtn.addEventListener("click", () => TrashTask(task.id));
+            const editBtn = document.getElementById(`edit-task-${task.id}`);
+            editBtn.addEventListener("click", () => EditTask(task.id, task.data()));
+        });
+    } catch (error) {
         //TODO show error toast
-        alert("User not signed in");
-        return;
+        console.log(error.message);
+        alert(error.message);
     }
+}
 
-    
+//update task
+async function EditTask(taskId, task) {
+    console.log(taskId, task.completed, task.task, task.priority);
+    document.getElementById("edit-input-task").value = task.task;
+    document.getElementById(task.priority === 1 ? "edit-low-priority" : task.priority === 2 ? "edit-medium-priority" : "edit-high-priority").checked = true;
 
+}
 
+//delete task
+async function TrashTask(taskId) {
+    try {
+        await DeleteTask(taskId);
+        ReadTasks();
+    } catch(error) {
+        //TODO show error toast
+        alert(error.message);
+    }
 }
 
 
@@ -98,6 +171,25 @@ async function LoadUserGreeting(user) {
 }
 
 
+async function ToggleTask(taskId) {
+    if(!CheckUser()) return;
+    
+    console.log("toggle task: ", taskId);
+
+    try {
+        const taskDoc = await getDoc(doc(db, "users", auth.currentUser.uid, "tasks", taskId));
+        if(taskDoc.exists()) {
+            const taskData = taskDoc.data();
+            const completed = !taskData.completed;
+            await updateDoc(doc(db, "users", auth.currentUser.uid, "tasks", taskId), {
+                completed: completed
+            });
+        }
+    } catch(error) {
+        alert(error.message);
+    }
+}
+
 
 
 
@@ -110,16 +202,11 @@ async function LoadUserGreeting(user) {
 
 function SetEventListeners() {
     //logout button
-    const logoutBtn = document.getElementById("logout-btn");
-    // console.log(logoutBtn);
-    logoutBtn.addEventListener("click", Logout);
-
+    logoutBtn.addEventListener("click", () => {});
+    // logoutBtn.addEventListener("click", Logout);
     //add task button
-    const addTaskBtn = document.getElementById("add-task-btn");
     addTaskBtn.addEventListener("click", AddTask);
     //Filter button
-
-
 }
 
 
@@ -128,7 +215,7 @@ async function LoadView(user) {
     LoadUserGreeting(user);
 
     //load tasks if user is logged in
-
+    ReadTasks();
 
     //set event listeners
     SetEventListeners();
@@ -141,9 +228,10 @@ async function handleAuthState() {
     onAuthStateChanged(auth, async (user) => {
         if (user) {
             //user is signed in
+            const test = CheckUser();
             LoadView(user);
         } else {
-            //redirect to the authentication page to sign in
+            console.log(user);
             window.location.href = "../html/index.html";
         }
     });
